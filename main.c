c983ff24421984f195ee54aba0b8b04fa08ebb55
Qamar Moavia <qmoavia.bee19seecs@seecs.edu.pk>
2024-10-31 11:24:45 +0300

    added main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    int sum = a + b;           // Calculate sum
+    int difference = a - b;    // Calculate difference
+
+    printf("Sum: %d\n", sum);           // Print sum
+    printf("Difference: %d\n", difference); // Print difference
+
+    return 0;
+}
